837e4a4fe78e8b3f320b3001f8ce4c00258b0368
JohannesSloth <joha218s@stud.kea.dk>
2022-09-11 15:37:05 +0200

    Reservations - more detailed info

--- src/main/java/dat3/cars/dto/ReservationResponse.java
+++ src/main/java/dat3/cars/dto/ReservationResponse.java
@@ -5,10 +5,16 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import dat3.cars.entity.Car;
 import dat3.cars.entity.Member;
 import dat3.cars.entity.Reservation;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ReservationResponse {
     int id;
